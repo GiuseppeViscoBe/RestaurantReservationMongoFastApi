dab8019180e5dfb3d6bbf21809e96d5012eee7d2
Giuseppe Visco <g.visco.ita@be-tse.it>
2025-11-11 08:18:42 +0100

    update mongo

--- app/db/db_async.py
+++ app/db/db_async.py
@@ -1,13 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient  # correct async Mongo client
+from pymongo import AsyncMongoClient
 import redis.asyncio as redis
 from app.config import settings
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # MongoDB
-    app.mongodb_client = AsyncIOMotorClient(settings.MONGO_URL)
+    app.mongodb_client = AsyncMongoClient(settings.MONGO_URL)
     app.database = app.mongodb_client[settings.DB_NAME]
     print("✅ Connected to MongoDB")
 
@@ -25,3 +25,4 @@ async def lifespan(app: FastAPI):
     await app.mongodb_client.close()
     await app.redis.close()
     print("🛑 Disconnected from MongoDB and Redis")
+
